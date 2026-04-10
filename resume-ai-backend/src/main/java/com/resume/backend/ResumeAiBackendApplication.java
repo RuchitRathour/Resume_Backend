3b2488f3d9ab4b736cc9decd42a6e78e82029e53
Ruchit Kumar <ruchitrathour@gmail.com>
2026-04-11 00:01:27 +0530

    Update CORS for new URL

--- resume-ai-backend/src/main/java/com/resume/backend/ResumeAiBackendApplication.java
+++ resume-ai-backend/src/main/java/com/resume/backend/ResumeAiBackendApplication.java
@@ -1,13 +1,3 @@
-//package com.resume.backend;
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//@SpringBootApplication
-//public class ResumeAiBackendApplication {
-//	public static void main(String[] args) {
-//		SpringApplication.run(ResumeAiBackendApplication.class, args);
-//	}
-//}
-
 package com.resume.backend;
 
 import org.springframework.boot.SpringApplication;
@@ -29,7 +19,10 @@ public class ResumeAiBackendApplication {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://tourmaline-zabaione-7b01b8.netlify.app")
+                        .allowedOrigins(
+                                "https://tourmaline-zabaione-7b01b8.netlify.app",
+                                "https://ai-resume-ruchit.netlify.app"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
